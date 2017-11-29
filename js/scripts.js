9c9dea31ac84af0b2ed71d95130af6aadb46694e
Abdullah Yahya and Kyle Tenzeldam <kyle@>
2017-11-29 10:10:28 -0800

    let someone submit text and capitalize it

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,11 @@ $(function() {
  $("#madLib").submit(function(event) {
    var firstName = $("input#firstName").val();
    var lastName = $("input#lastName").val();
-   var verb = $("input#verb").val();
-   var noun = $("input#noun").val();
-   var city = $("input#city").val();
+   var fullName = firstName.concat(lastName);
+   var capName = fullName.toUpperCase();
 
-   $(".firstName").append(firstName);
+   $(".fullName").append(capName);
    $(".lastName").append(lastName);
-   $(".verb").append(verb);
-   $(".noun").append(noun);
-   $(".city").append(city);
 
    $("#story").show();
 
